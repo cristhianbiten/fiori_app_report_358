e84e06d80f3847f79fb89bd874845b70edf0c746
Cristhian Bitencourt <cristhian.bitencourt@outlook.com>
2024-04-21 18:48:25 +0000

    Add frag category

--- webapp/controller/Lista.controller.js
+++ webapp/controller/Lista.controller.js
@@ -2,12 +2,13 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
-    "br/com/gestao/fioriappreport358/util/Formatter"
+    "br/com/gestao/fioriappreport358/util/Formatter",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, Filter, FilterOperator, Formatter) {
+    function (Controller, Filter, FilterOperator, Formatter, Fragment) {
         "use strict";
 
         return Controller.extend("br.com.gestao.fioriappreport358.controller.Lista", {
@@ -27,23 +28,31 @@ sap.ui.define([
                 //Capturando individulamente cada objeto Input do objeto Filter Bar
                 var oProdutoInput = this.getView().byId("productIDInput");
                 var oProdutoNomeInput = this.getView().byId("productNameInput");
+                var oCategoriaInput = this.getView().byId("productCategoryInput");
+
+                var objFilter = { filters: [], and: true };
+                objFilter.filters.push(new Filter("Productid", FilterOperator.Contains, oProdutoInput.getValue()));
+                objFilter.filters.push(new Filter("Name", FilterOperator.Contains, oProdutoNomeInput.getValue()));
+                objFilter.filters.push(new Filter("Category", FilterOperator.Contains, oCategoriaInput.getValue()));
 
                 //Cria o objeto Filter baseado no valor do objeto
-                var oFilter = new Filter({
-                    filters: [
-                        new Filter({
-                            path: "Productid",
-                            operator: FilterOperator.Contains,
-                            value1: oProdutoInput.getValue(),
-                        }),
-                        new Filter({
-                            path: "Name",
-                            operator: FilterOperator.Contains,
-                            value1: oProdutoNomeInput.getValue(),
-                        }),
-                    ],
-                    and: true
-                })
+                var oFilter = new Filter(objFilter);
+
+                /*                 var oFilter = new Filter({
+                                    filters: [
+                                        new Filter({
+                                            path: "Productid",
+                                            operator: FilterOperator.Contains,
+                                            value1: oProdutoInput.getValue(),
+                                        }),
+                                        new Filter({
+                                            path: "Name",
+                                            operator: FilterOperator.Contains,
+                                            value1: oProdutoNomeInput.getValue(),
+                                        }),
+                                    ],
+                                    and: true
+                                }) */
 
                 //Busca objeto Table
                 var oTable = this.getView().byId("tableProdutos");
@@ -76,6 +85,73 @@ sap.ui.define([
                 var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                 oRouter.navTo("Detalhes", { productId: oProductId });
 
+            },
+            onCategoria: function (oEvent) {
+
+                this._oInput = oEvent.getSource().getId();
+                var oView = this.getView();
+
+                //Verifico se o objeto fragment existe. Se não, crio e adiciona na View
+                if (!this._CategoriaSerchHelp) {
+                    this._CategoriaSerchHelp = Fragment.load({
+                        id: oView.getId(),
+                        name: "br.com.gestao.fioriappreport358.frags.SH_Categorias",
+                        controller: this
+                    }).then(function (oDialog) {
+                        oView.addDependent(oDialog);
+                        return oDialog;
+                    });
+                }
+
+                this._CategoriaSerchHelp.then(function (oDialog) {
+
+                    //Limpando o filtro de categorias na abertura do fragment
+                    oDialog.getBinding("items").filter([]);
+
+                    //Abertura do Fragment
+                    oDialog.open();
+                })
+
+            },
+
+            onValueHelpSearch: function (oEvent) {
+
+                //Capturando o valor digitado pelo usuário
+                var sValue = oEvent.getParameter("value");
+
+                //Opção 1 - Cria um único objeto filtro
+                //Criando um objeto do tipo Filter que irá receber o valor e associar na propriedade Description
+                //var oFilter = new Filter("Description", FilterOperator.Contains, sValue)
+                //oEvent.getSource().getBinding("items").filter([oFilter]);
+
+                //Opção 2- Podemos criar um objeto (dinamico) onde adiciono várias propriedades
+                var objFilter = { filters: [], and: false };
+                objFilter.filters.push(new Filter("Description", FilterOperator.Contains, sValue));
+                objFilter.filters.push(new Filter("Category", FilterOperator.Contains, sValue));
+
+                var oFilter = new Filter(objFilter);
+                oEvent.getSource().getBinding("items").filter(oFilter);
+
+            },
+
+            onValueHelpClose: function (oEvent) {
+
+                var oSelectedItem = oEvent.getParameter("selectedItem");
+                var oInput = null;
+
+                //Verifica se existe um objeto para o id correspondente para o this._oInput, caso sim, ele cria a referencia
+                if (this.byId(this._oInput)) {
+                    oInput = this.byId(this._oInput);
+                } else {
+                    oInput = sap.ui.getCore().byId(this._oInput);
+                }
+
+                if (!oSelectedItem) {
+                    oInput.resetProperty("value");
+                    return;
+                }
+
+                oInput.setValue(oSelectedItem.getTitle());
             }
 
         });
